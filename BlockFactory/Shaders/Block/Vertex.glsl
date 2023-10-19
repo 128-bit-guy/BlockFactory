aeeb47e96519fd6be0311a68a9c15f565da2708c
128-bit-guy <44749584+128-bit-guy@users.noreply.github.com>
2023-10-19 12:45:50 +0300

    Change fake sphericalness function

--- BlockFactory/Shaders/Block/Vertex.glsl
+++ BlockFactory/Shaders/Block/Vertex.glsl
@@ -13,17 +13,19 @@ const float planetRadius = 300f;
 out vec4 vertexColor; // specify a color output to the fragment shader
 out vec2 vertexUv;
 
+vec3 spherify(vec3 mp) {
+    mp -= playerPos;
+    float dist = length(mp.xz);
+    float delta = planetRadius - sqrt(planetRadius * planetRadius - dist * dist);
+    mp.y -= delta;
+    mp += playerPos;
+    return mp;
+}
+
 void main()
 {
     vec3 mp = (model * vec4(aPos, 1.0)).xyz;
-    mp.xz -= playerPos.xz;
-    float dist = length(mp.xz);
-    float angle = dist / planetRadius;
-    vec2 up = vec2(sin(angle), cos(angle));
-    mp.xz /= dist;
-    mp.xz *= up.x * (mp.y + planetRadius);
-    mp.y = (up.y * (mp.y + planetRadius)) - planetRadius;
-    mp.xz += playerPos.xz;
+    mp = spherify(mp);
     gl_Position = projection * view * vec4(mp, 1.0); // see how we directly give a vec3 to vec4's constructor
     vertexColor = color;
     vertexUv = uv;
