d285e4512501adfb1f956c035269aa837baff822
128-bit-guy <44749584+128-bit-guy@users.noreply.github.com>
2023-10-23 23:08:52 +0300

    Use differnet noise library, and add mountains using riged noise

--- BlockFactory/World_/Gen/BaseTerrainGenerator.cs
+++ BlockFactory/World_/Gen/BaseTerrainGenerator.cs
@@ -1,22 +1,23 @@
 ﻿using BlockFactory.Base;
 using BlockFactory.Math_;
-using SharpNoise.Modules;
 using Silk.NET.Maths;
 
 namespace BlockFactory.World_.Gen;
 
 public class BaseTerrainGenerator
 {
-    private readonly Perlin _perlin = new();
+    private readonly FastNoiseLite _noise = new FastNoiseLite();
+    private readonly FastNoiseLite _mountainNoise = new FastNoiseLite();
 
     public void GenerateBaseTerrain(Chunk c)
     {
+        _mountainNoise.SetFractalType(FastNoiseLite.FractalType.Ridged);
         for (var i = 0; i < Constants.ChunkSize; ++i)
         for (var k = 0; k < Constants.ChunkSize; ++k)
         {
             var x = i + c.Position.ShiftLeft(Constants.ChunkSizeLog2).X;
             var z = k + c.Position.ShiftLeft(Constants.ChunkSizeLog2).Z;
-            var val = _perlin.GetValue(x / 10.0f, z / 10.0f, 0);
+            var val = _noise.GetNoise(x * 3.0d, z * 3.0d) + Math.Max(0, _mountainNoise.GetNoise(x / 10.0d, z / 10.0d)) * 50;
             for (var j = 0; j < Constants.ChunkSize; ++j)
             {
                 var y = j + c.Position.ShiftLeft(Constants.ChunkSizeLog2).Y;
