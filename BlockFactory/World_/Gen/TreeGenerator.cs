9bf300494e43474d6e65f2c34bd855596ae269a6
128-bit-guy <44749584+128-bit-guy@users.noreply.github.com>
2024-02-10 15:17:21 +0300

    Make tree trunks thicker

--- BlockFactory/World_/Gen/TreeGenerator.cs
+++ BlockFactory/World_/Gen/TreeGenerator.cs
@@ -1,6 +1,5 @@
 ﻿using BlockFactory.Block_;
 using BlockFactory.Random_;
-using BlockFactory.World_.Interfaces;
 using Silk.NET.Maths;
 
 namespace BlockFactory.World_.Gen;
@@ -9,41 +8,33 @@ public static class TreeGenerator
 {
     public static void Generate(BlockPointer pointer, Random rng)
     {
-        GenerateBranch(pointer, Vector3D<float>.UnitY, Vector3D<float>.Zero, rng, 20);
-        // var direction = Vector3D<float>.UnitY;
-        // var relPos = Vector3D<float>.Zero;
-        // Span<Vector3D<int>> s = stackalloc Vector3D<int>[4];
-        // s[0] = Vector3D<int>.UnitX;
-        // s[1] = Vector3D<int>.UnitZ;
-        // s[2] = -Vector3D<int>.UnitX;
-        // s[3] = -Vector3D<int>.UnitZ;
-        // s.Shuffle(rng);
-        // for (var i = 0; i < 20; ++i)
-        // {
-        //     var np = pointer + new Vector3D<float>(MathF.Round(relPos.X), MathF.Round(relPos.Y),
-        //         MathF.Round(relPos.Z)).As<int>();
-        //     for (int k = 0; k < 2; ++k)
-        //     {
-        //         var np2 = np + new Vector3D<int>(0, k, 0);
-        //         np2.SetBlock(Blocks.Log);
-        //         for (int j = 0; j < ((20 - i) / 6) - 1; ++j)
-        //         {
-        //             (np2 + s[j]).SetBlock(Blocks.Log);
-        //         }
-        //     }
-        //
-        //     if (i > 15)
-        //     {
-        //         SetLeaves(np, 4);
-        //     }
-        //
-        //     relPos += direction / 2;
-        //     var newDir = 4 * direction + 0 * RandomUtils.PointOnSphere(rng) + Vector3D<float>.UnitY;
-        //     direction = Vector3D.Normalize(newDir);
-        // }
+        Span<Vector3D<int>> neighbours = stackalloc Vector3D<int>[4];
+        neighbours[0] = Vector3D<int>.UnitX;
+        neighbours[1] = Vector3D<int>.UnitZ;
+        neighbours[2] = -Vector3D<int>.UnitX;
+        neighbours[3] = -Vector3D<int>.UnitZ;
+        neighbours.Shuffle(rng);
+        GenerateBranch(pointer, Vector3D<float>.UnitY, Vector3D<float>.Zero, rng, 25, neighbours);
+        for (var i = 0; i < 4; ++i)
+        {
+            var neighbour = pointer + neighbours[i] - Vector3D<int>.UnitY;
+            for (var j = 0; j < 3; ++j)
+            {
+                var neighbourBottom = neighbour - new Vector3D<int>(0, j, 0);
+                if (neighbourBottom.GetBlock() == 0 || neighbourBottom.GetBlock() == Blocks.Leaves.Id)
+                {
+                    neighbourBottom.SetBlock(Blocks.Log);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
     }
 
-    private static void GenerateBranch(BlockPointer pointer, Vector3D<float> direction, Vector3D<float> relPos, Random rng, int leftLength)
+    private static void GenerateBranch(BlockPointer pointer, Vector3D<float> direction, Vector3D<float> relPos,
+        Random rng, int leftLength, Span<Vector3D<int>> neighbours)
     {
         var first = true;
         while (leftLength > 0)
@@ -51,20 +42,22 @@ public static class TreeGenerator
             if (first)
             {
                 first = false;
-            } else if (rng.Next(80) < leftLength)
-            {
-                GenerateBranch(pointer, direction, relPos, rng, leftLength);
             }
+            else if (rng.Next(80) < leftLength)
+            {
+                GenerateBranch(pointer, direction, relPos, rng, leftLength, neighbours);
+            }
+
             var np = pointer + new Vector3D<float>(MathF.Round(relPos.X), MathF.Round(relPos.Y),
                 MathF.Round(relPos.Z)).As<int>();
             for (int k = 0; k < 2; ++k)
             {
                 var np2 = np + new Vector3D<int>(0, k, 0);
                 np2.SetBlock(Blocks.Log);
-                // for (int j = 0; j < ((20 - i) / 6) - 1; ++j)
-                // {
-                //     (np2 + s[j]).SetBlock(Blocks.Log);
-                // }
+                for (int j = 0; j < Math.Min(4, (leftLength - 12) / 2 - 1); ++j)
+                {
+                    (np2 + neighbours[j]).SetBlock(Blocks.Log);
+                }
             }
 
             if (leftLength < 3)
