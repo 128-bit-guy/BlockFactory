89f611db45a79d6aaafa2bda5cec18eab7f0bd35
128-bit-guy <44749584+128-bit-guy@users.noreply.github.com>
2024-05-03 20:01:07 +0300

    Fix pixels with zero alpha when loading texture atlas

--- BlockFactory/Client/Render/Texture_/TextureAtlas.cs
+++ BlockFactory/Client/Render/Texture_/TextureAtlas.cs
@@ -1,4 +1,5 @@
-﻿using BlockFactory.Base;
+﻿using System.Drawing;
+using BlockFactory.Base;
 using Silk.NET.Maths;
 using Silk.NET.OpenGL;
 
@@ -20,6 +21,7 @@ public class TextureAtlas : IDisposable
         var spriteH = image.Height >> sizeLog2;
         var nImage = new Image(image.Width << 1, image.Height << 1);
         _spriteBoxes = new Box2D<float>[1 << (sizeLog2 << 1)];
+        Span<long> sumColor = stackalloc long[4];
         for (var i = 0; i < 1 << sizeLog2; ++i)
         for (var j = 0; j < 1 << sizeLog2; ++j)
         {
@@ -27,6 +29,37 @@ public class TextureAtlas : IDisposable
             var maxSx = ((i + 1) << 1) * spriteW;
             var minSy = ((j << 1) | 1) * spriteH;
             var maxSy = ((j + 1) << 1) * spriteH;
+            sumColor.Fill(0);
+            for(var k = i * spriteW; k < (i + 1) * spriteW; ++k)
+            for (var l = j * spriteH; l < (j + 1) * spriteH; ++l)
+            {
+                var col = image[k, l];
+                var argb = col.ToArgb();
+                var a = (int)((uint)argb >> 24);
+                var rgb = argb & ((1 << 24) - 1);
+                for (var m = 0; m < 3; ++m)
+                {
+                    var comp = (rgb >> (8 * m)) & ((1 << 8) - 1);
+                    sumColor[m] += comp * a;
+                }
+
+                sumColor[3] += a;
+            }
+
+            Color mColor;
+            {
+                var mArgb = 0;
+                if (sumColor[3] != 0)
+                {
+                    for (var m = 0; m < 3; ++m)
+                    {
+                        var comp = (int)(sumColor[m] / sumColor[3]);
+                        mArgb |= (comp << (8 * m));
+                    }
+                }
+
+                mColor = Color.FromArgb(mArgb);
+            }
             var maxPx = Math.Min((((i + 1) << 1) | 1) * spriteW - (spriteW >> 1), nImage.Width);
             var maxPy = Math.Min((((j + 1) << 1) | 1) * spriteH - (spriteH >> 1), nImage.Height);
             for (var x = ((i << 1) | 1) * spriteW - (spriteW >> 1);
@@ -40,7 +73,12 @@ public class TextureAtlas : IDisposable
                 var clampedY = Math.Clamp(y, minSy, maxSy - 1);
                 var ox = clampedX - ((i << 1) | 1) * spriteW + i * spriteW;
                 var oy = clampedY - ((j << 1) | 1) * spriteH + j * spriteH;
-                nImage[x, y] = image[ox, oy];
+                var oColor = image[ox, oy];
+                if (oColor.A == 0)
+                {
+                    oColor = mColor;
+                }
+                nImage[x, y] = oColor;
             }
 
             _spriteBoxes[i | (((1 << sizeLog2) - j - 1) << sizeLog2)] = new Box2D<float>(
